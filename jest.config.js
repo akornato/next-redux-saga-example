a8542617f2aecb32691dd53bc80963b99c1e6eb4
akornato <artur.kornatowski@gmail.com>
2018-12-01 16:46:38 +0100

    change coverage folder

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  coverageDirectory: 'static/coverage',
   setupFiles: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
 };
