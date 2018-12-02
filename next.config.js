2394c4d4d0284cf4481ae7a96a45afd5c4a0e88e
akornato <artur.kornatowski@gmail.com>
2018-12-02 11:52:25 +0100

    reconfigure tailwindcss

--- next.config.js
+++ next.config.js
@@ -6,25 +6,10 @@ module.exports = withCSS({
   },
   publicRuntimeConfig: {
     // Will be available on both server and client
-    gitHubToken: '23d380f73d94740763afbf2ddda955cca409f199',
   },
   exportPathMap: function() {
     return {
       '/': { page: '/' },
     };
-  },
-  webpack(config, { isServer, buildId, dev }) {
-    if (!isServer) {
-      config.module.rules
-        .find(({ test }) => test.test('style.css'))
-        .use.push({
-          loader: 'css-purify-webpack-loader',
-          options: {
-            includes: ['./pages/*.js', './components/*.js'],
-          },
-        });
-    }
-
-    return config;
   },
 });
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 
-import '../static/css/vendor/tailwind.css'; // PurifyCSS-ed in next.config.js
+import '../styles/index.css';
 
 export default class AppWithRedux extends App {
   render() {
