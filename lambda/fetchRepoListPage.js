c6b1973c600b741d5559d51bfb777047e162521b
akornato <artur.kornatowski@gmail.com>
2018-12-06 16:24:38 +0100

    refactor lambda to async await

--- lambda/fetchRepoListPage.js
+++ lambda/fetchRepoListPage.js
@@ -1,46 +1,35 @@
 import axios from 'axios';
 require('dotenv').config();
 
-export function handler(event, context, callback) {
-  axios({
-    url: 'https://api.github.com/graphql',
-    method: 'post',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.GITHUB_GRAPHQL_API_TOKEN}`,
-    },
-    data: JSON.parse(event.body),
-  })
-    .then(function(response) {
-      callback(null, {
-        statusCode: response.status,
-        headers: response.headers,
-        body: JSON.stringify(response.data),
-      });
-    })
-    .catch(function(error) {
-      if (error.response) {
-        callback(null, {
-          statusCode: error.response.status,
-          headers: error.response.headers,
-          body: JSON.stringify(error.response.data),
-        });
-      } else if (error.request) {
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-        callback(null, {
-          statusCode: 404,
-          body: error.message,
-        });
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log(error.message);
-        callback(null, {
-          statusCode: 500,
-          body: error.message,
-        });
-      }
+export async function handler(event) {
+  try {
+    const response = await axios({
+      url: 'https://api.github.com/graphql',
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.GITHUB_GRAPHQL_API_TOKEN}`,
+      },
+      data: JSON.parse(event.body),
     });
+    return {
+      statusCode: response.status,
+      headers: response.headers,
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    if (error.response) {
+      return {
+        statusCode: error.response.status,
+        headers: error.response.headers,
+        body: JSON.stringify(error.response.data),
+      };
+    } else {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: error.message }),
+      };
+    }
+  }
 }
