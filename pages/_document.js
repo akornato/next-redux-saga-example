cbc2ba94e1b5d6058b14b53c4f1193f2030491dc
akornato <artur.kornatowski@gmail.com>
2019-04-06 23:36:40 +0200

    Add babel and fetch polyfills

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,14 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
           <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/antd/3.10.9/antd.min.css" />
+          <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/7.2.5/polyfill.min.js"
+            noModule="nomodule"
+          />
+          <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/fetch/2.0.4/fetch.min.js"
+            noModule="nomodule"
+          />
         </Head>
         <body>
           <Main />
